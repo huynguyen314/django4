cb11f075ffde12b5ba911ddc1aaefa0d99b00c79
huynguyen314 <thanhhuya10lk@gmail.com>
2022-12-22 22:42:23 +0700

    Weighting queries

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -121,12 +121,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body', config='spanish')
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B', config='spanish')
             search_query = SearchQuery(query, config='spanish')
             results = Post.published.annotate(
                 search=search_vector,
                 rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
+            ).filter(rank__gte=0.3).order_by('-rank')
     
     return render(request, 
                   'blog/post/search.html', 
