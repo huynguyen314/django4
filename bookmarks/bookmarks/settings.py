c9052c90596ecbd8a6a3bb0e9f5a9e87e57c591f
huynguyen314 <thanhhuya10lk@gmail.com>
2022-12-27 21:58:54 +0700

    Create a profile for users that register with social authentication

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -145,3 +145,16 @@ SOCIAL_AUTH_FACEBOOK_SECRET = 'c8678a94abad012b0f7626461f300a62' # Facebook App 
 # SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '29018384835-d0fap71ecj5c01da6enq43af0kab2oju.apps.googleusercontent.com' # Google Client ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
